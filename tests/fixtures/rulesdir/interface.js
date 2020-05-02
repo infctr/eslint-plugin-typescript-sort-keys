22abb3df4d925ee23b431e03fa1b37106b2f1280
infctr <infctr@gmail.com>
2020-05-02 22:32:23 +0300

    Add fixture wrapper for rulesdir/autofix test

---
+++ tests/fixtures/rulesdir/interface.js
@@ -0,0 +1,7 @@
+const { rule } = require('../source/interface')
+
+/**
+ * `module.exports` is required for `eslint --rulesdir` interop in autofix tests
+ * @see https://github.com/eslint/eslint/issues/13232
+ */
+module.exports = rule
---
+++ tests/fixtures/rulesdir/string-enum.js
@@ -0,0 +1,7 @@
+const { rule } = require('../source/string-enum')
+
+/**
+ * `module.exports` is required for `eslint --rulesdir` interop in autofix tests
+ * @see https://github.com/eslint/eslint/issues/13232
+ */
+module.exports = rule
